248354b968567fdc88fdf1f9d3a87bfc06786531
Fruitloopins <49851457+Fruitloopins@users.noreply.github.com>
2023-08-13 02:46:37 +1000

    Reimplement "enabled" message

--- src/main/java/net/earthmc/bspawn/listeners/TownTogglePublicListener.java
+++ src/main/java/net/earthmc/bspawn/listeners/TownTogglePublicListener.java
@@ -1,10 +1,12 @@
 package net.earthmc.bspawn.listeners;
 
 import com.palmergames.bukkit.towny.TownyEconomyHandler;
+import com.palmergames.bukkit.towny.TownyMessaging;
 import com.palmergames.bukkit.towny.confirmations.Confirmation;
 import com.palmergames.bukkit.towny.confirmations.ConfirmationTransaction;
 import com.palmergames.bukkit.towny.event.town.toggle.TownTogglePublicEvent;
 import com.palmergames.bukkit.towny.object.Town;
+import com.palmergames.bukkit.towny.object.Translatable;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -24,8 +26,11 @@ public class TownTogglePublicListener implements Listener {
             Town town = event.getTown();
 
             Confirmation
-                    .runOnAccept(() -> town.setPublic(true))
-                    .setTitle("You must pay to toggle public to true. This will cost " + TownyEconomyHandler.getFormattedBalance(config.getInt("toggleCost")) + " and will require " + TownyEconomyHandler.getFormattedBalance(config.getInt("upkeepCost")) + " daily upkeep to remain true.")
+                    .runOnAccept(() -> {
+                        town.setPublic(true);
+                        TownyMessaging.sendPrefixedTownMessage(town, Translatable.of("msg_changed_public", "enabled"));
+                    })
+                    .setTitle("You must pay to toggle public to true. This will cost " + TownyEconomyHandler.getFormattedBalance(config.getInt("toggleCost")) + " and will require " + TownyEconomyHandler.getFormattedBalance(config.getInt("upkeepCost")) + " daily upkeep each new day to remain true.")
                     .setCost(new ConfirmationTransaction(() -> config.getDouble("toggleCost"), town.getAccount(), "Cost of toggling public to true."))
                     .sendTo(event.getSender());
         }
