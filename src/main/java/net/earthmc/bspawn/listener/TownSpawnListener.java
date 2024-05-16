79937930d48ba62b55b4bfce7da5c265c292c587
Fruitloopins <49851457+jwkerr@users.noreply.github.com>
2024-05-17 08:29:46 +1000

    Allow town spawns if user has towny spawn admin node

--- src/main/java/net/earthmc/bspawn/listener/TownSpawnListener.java
+++ src/main/java/net/earthmc/bspawn/listener/TownSpawnListener.java
@@ -1,10 +1,12 @@
 package net.earthmc.bspawn.listener;
 
 import com.palmergames.bukkit.towny.TownyAPI;
+import com.palmergames.bukkit.towny.TownyUniverse;
 import com.palmergames.bukkit.towny.event.TownSpawnEvent;
 import com.palmergames.bukkit.towny.object.Nation;
 import com.palmergames.bukkit.towny.object.Resident;
 import com.palmergames.bukkit.towny.object.Town;
+import com.palmergames.bukkit.towny.permissions.PermissionNodes;
 import net.earthmc.bspawn.manager.TownMetadataManager;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -13,10 +15,13 @@ public class TownSpawnListener implements Listener {
 
     @EventHandler
     public void onTownSpawn(TownSpawnEvent event) {
-        Town toTown = event.getToTown();
-
         Resident resident = TownyAPI.getInstance().getResident(event.getPlayer());
         if (resident == null) return;
+
+        if (TownyUniverse.getInstance().getPermissionSource().testPermission(event.getPlayer(), PermissionNodes.TOWNY_SPAWN_ADMIN.getNode()))
+            return;
+
+        Town toTown = event.getToTown();
 
         Nation toNation = toTown.getNationOrNull();
 
